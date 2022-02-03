b70a1add21abe6effb8bd5cc0192289e7c8c6fb5
mo-hokage1 <80471511+mo-hokage1@users.noreply.github.com>
2022-02-03 14:14:43 +0000

    Add files via upload

---
+++ module4-solution/goodbye.js
@@ -0,0 +1,10 @@
+(function (window){
+    let byeSpeaker = {};
+    let speakWord = "Good Bye";
+    
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.byeSpeaker = byeSpeaker;
+    })(window);
